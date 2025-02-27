46afb470fcf40b35673eb8832eaf4dadfeca1dd7
fajar <fajar.nurrohman@mastersystem.co.id>
2025-02-27 16:57:29 +0700

    add aws load balancer to eks

--- 04_jboss.tf
+++ 04_jboss.tf
@@ -1,181 +1,181 @@
-locals {
-  ec2_jboss = <<-EOF
-              #!/bin/bash
-              sudo hostnamectl set-hostname jboss
-              sleep 5s
-              sudo yum install java-11-openjdk-devel unzip -y
-              sleep 5s
-              curl -sLO "https://download1507.mediafire.com/om1ino5sdtfgTwNIMamWu1tXbDSwaKeq_ARChmRc71p6ddMxTLzB4Pm9Di4rGzUvYmV0VLB4mmwsMyE_WqvxVvrPlGR7G1PB1Ol4gkCXp_BDY0EBLSDqYV_Jziik5q2ILD6jQrRYsNrxBx6MxLCdVoVKLhAo1qymCqe6fexqCyJlHg/oq90v78d8u30w2u/jboss-eap-8.0.0.zip"
-              sleep 5s
-              unzip jboss-eap-8.0.0.zip
-              mv jboss-eap-8.0 jboss
-              sleep 5s
-              sudo setenforce 0
-              sleep 5s
-              sudo chmod +x /home/ec2-user/jboss/bin/standalone.sh
-              sleep 5s
-              export JAVA_HOME=/bin/java
-              export JBOSS_HOME=/home/ec2-user/jboss
-              sleep 5s
-              cd /home/ec2-user/jboss/bin/
-              sudo ./add-user.sh -u admin -p P@ssw0rd123 -g admin
-              sudo ./add-user.sh -u jboss -p P@ssw0rd123 -g admin
-              sleep 5s
-              sudo sed -i 's/$${jboss.bind.address.management:127.0.0.1}/$${jboss.bind.address.management:0.0.0.0}/g' /home/ec2-user/jboss/standalone/configuration/standalone.xml
-              sudo sed -i 's/$${jboss.bind.address:127.0.0.1}/$${jboss.bind.address:0.0.0.0}/g' /home/ec2-user/jboss/standalone/configuration/standalone.xml
-              sleep 5s
-              echo "
-                [Unit]
-                Description=JBoss EAP 8
-                After=network.target
+# locals {
+#   ec2_jboss = <<-EOF
+#               #!/bin/bash
+#               sudo hostnamectl set-hostname jboss
+#               sleep 5s
+#               sudo yum install java-11-openjdk-devel unzip -y
+#               sleep 5s
+#               curl -sLO "https://download1507.mediafire.com/om1ino5sdtfgTwNIMamWu1tXbDSwaKeq_ARChmRc71p6ddMxTLzB4Pm9Di4rGzUvYmV0VLB4mmwsMyE_WqvxVvrPlGR7G1PB1Ol4gkCXp_BDY0EBLSDqYV_Jziik5q2ILD6jQrRYsNrxBx6MxLCdVoVKLhAo1qymCqe6fexqCyJlHg/oq90v78d8u30w2u/jboss-eap-8.0.0.zip"
+#               sleep 5s
+#               unzip jboss-eap-8.0.0.zip
+#               mv jboss-eap-8.0 jboss
+#               sleep 5s
+#               sudo setenforce 0
+#               sleep 5s
+#               sudo chmod +x /home/ec2-user/jboss/bin/standalone.sh
+#               sleep 5s
+#               export JAVA_HOME=/bin/java
+#               export JBOSS_HOME=/home/ec2-user/jboss
+#               sleep 5s
+#               cd /home/ec2-user/jboss/bin/
+#               sudo ./add-user.sh -u admin -p P@ssw0rd123 -g admin
+#               sudo ./add-user.sh -u jboss -p P@ssw0rd123 -g admin
+#               sleep 5s
+#               sudo sed -i 's/$${jboss.bind.address.management:127.0.0.1}/$${jboss.bind.address.management:0.0.0.0}/g' /home/ec2-user/jboss/standalone/configuration/standalone.xml
+#               sudo sed -i 's/$${jboss.bind.address:127.0.0.1}/$${jboss.bind.address:0.0.0.0}/g' /home/ec2-user/jboss/standalone/configuration/standalone.xml
+#               sleep 5s
+#               echo "
+#                 [Unit]
+#                 Description=JBoss EAP 8
+#                 After=network.target
 
-                [Service]
-                User=ec2-user
-                Group=ec2-user
-                ExecStart=/home/ec2-user/jboss/bin/standalone.sh
-                ExecStop=/home/ec2-user/jboss/bin/jboss-cli.sh --connect command=:shutdown
-                WorkingDirectory=/home/ec2-user/jboss
-                LimitNOFILE=1024
-                TimeoutSec=300
+#                 [Service]
+#                 User=ec2-user
+#                 Group=ec2-user
+#                 ExecStart=/home/ec2-user/jboss/bin/standalone.sh
+#                 ExecStop=/home/ec2-user/jboss/bin/jboss-cli.sh --connect command=:shutdown
+#                 WorkingDirectory=/home/ec2-user/jboss
+#                 LimitNOFILE=1024
+#                 TimeoutSec=300
 
-                [Install]
-                WantedBy=multi-user.target" | sudo tee /etc/systemd/system/jboss.service
-              sleep 5s
-              sudo systemctl daemon-reload
-              sudo systemctl enable jboss.service
-              sudo systemctl start jboss.service
-              EOF
-}
+#                 [Install]
+#                 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/jboss.service
+#               sleep 5s
+#               sudo systemctl daemon-reload
+#               sudo systemctl enable jboss.service
+#               sudo systemctl start jboss.service
+#               EOF
+# }
 
-resource "random_integer" "subnet_index" {
-  min = 0
-  max = length(module.vpc.public_subnets) - 1
-}
+# resource "random_integer" "subnet_index" {
+#   min = 0
+#   max = length(module.vpc.public_subnets) - 1
+# }
 
-module "jboss" {
-  source = "terraform-aws-modules/ec2-instance/aws"
+# module "jboss" {
+#   source = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "jboss"
+#   name = "jboss"
 
-  instance_type          = "m5.2xlarge"
-  key_name               = "key"
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = element(module.vpc.public_subnets, random_integer.subnet_index.result)
-  ami                    = "ami-0b748249d064044e8" # rhel 9
+#   instance_type          = "m5.2xlarge"
+#   key_name               = "key"
+#   vpc_security_group_ids = [module.vpc.default_security_group_id]
+#   subnet_id              = element(module.vpc.public_subnets, random_integer.subnet_index.result)
+#   ami                    = "ami-0b748249d064044e8" # rhel 9
 
-  associate_public_ip_address = true
-  user_data                   = local.ec2_jboss
+#   associate_public_ip_address = true
+#   user_data                   = local.ec2_jboss
 
-  root_block_device = [
-    {
-      volume_size = 30
-      volume_type = "gp3"
-    }
-  ]
+#   root_block_device = [
+#     {
+#       volume_size = 30
+#       volume_type = "gp3"
+#     }
+#   ]
 
-  depends_on = [module.vpc]
+#   depends_on = [module.vpc]
 
-  tags = {
-    Terraform = "true"
-  }
-}
+#   tags = {
+#     Terraform = "true"
+#   }
+# }
 
-module "jboss_alb" {
-  source = "terraform-aws-modules/alb/aws"
+# module "jboss_alb" {
+#   source = "terraform-aws-modules/alb/aws"
 
-  name = "jboss"
+#   name = "jboss"
 
-  load_balancer_type         = "application"
-  vpc_id                     = module.vpc.vpc_id
-  subnets                    = module.vpc.public_subnets
-  security_groups            = [module.vpc.default_security_group_id]
-  enable_deletion_protection = false
+#   load_balancer_type         = "application"
+#   vpc_id                     = module.vpc.vpc_id
+#   subnets                    = module.vpc.public_subnets
+#   security_groups            = [module.vpc.default_security_group_id]
+#   enable_deletion_protection = false
 
-  target_groups = {
-    jboss = {
-      name_prefix      = "jeb"
-      backend_protocol = "HTTP"
-      backend_port     = 8080
-      target_type      = "instance"
+#   target_groups = {
+#     jboss = {
+#       name_prefix      = "jeb"
+#       backend_protocol = "HTTP"
+#       backend_port     = 8080
+#       target_type      = "instance"
 
-      health_check = {
-        enabled             = true
-        interval            = 30
-        path                = "/"
-        port                = "traffic-port"
-        healthy_threshold   = 3
-        unhealthy_threshold = 3
-        timeout             = 6
-        protocol            = "HTTP"
-      }
+#       health_check = {
+#         enabled             = true
+#         interval            = 30
+#         path                = "/"
+#         port                = "traffic-port"
+#         healthy_threshold   = 3
+#         unhealthy_threshold = 3
+#         timeout             = 6
+#         protocol            = "HTTP"
+#       }
 
-      protocol_version = "HTTP1"
-      target_id        = module.jboss.id
-      port             = 8080
-      tags = {
-        Name = "jboss"
-      }
-    }
+#       protocol_version = "HTTP1"
+#       target_id        = module.jboss.id
+#       port             = 8080
+#       tags = {
+#         Name = "jboss"
+#       }
+#     }
 
-    jboss-mgmt = {
-      name_prefix      = "jeb"
-      backend_protocol = "HTTP"
-      backend_port     = 9990
-      target_type      = "instance"
+#     jboss-mgmt = {
+#       name_prefix      = "jeb"
+#       backend_protocol = "HTTP"
+#       backend_port     = 9990
+#       target_type      = "instance"
 
-      health_check = {
-        enabled             = true
-        interval            = 30
-        path                = "/"
-        port                = "traffic-port"
-        healthy_threshold   = 3
-        unhealthy_threshold = 3
-        timeout             = 6
-        protocol            = "HTTP"
-      }
+#       health_check = {
+#         enabled             = true
+#         interval            = 30
+#         path                = "/"
+#         port                = "traffic-port"
+#         healthy_threshold   = 3
+#         unhealthy_threshold = 3
+#         timeout             = 6
+#         protocol            = "HTTP"
+#       }
 
-      protocol_version = "HTTP1"
-      target_id        = module.jboss.id
-      port             = 9990
-      tags = {
-        Name = "jboss-mgmt"
-      }
-    }
-  }
+#       protocol_version = "HTTP1"
+#       target_id        = module.jboss.id
+#       port             = 9990
+#       tags = {
+#         Name = "jboss-mgmt"
+#       }
+#     }
+#   }
 
-  listeners = {
-    http = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_key = "jboss"
-      }
-    }
-    mgmnt = {
-      port     = 9990
-      protocol = "HTTP"
-      forward = {
-        target_group_key = "jboss-mgmt"
-      }
-    }
-  }
+#   listeners = {
+#     http = {
+#       port     = 80
+#       protocol = "HTTP"
+#       forward = {
+#         target_group_key = "jboss"
+#       }
+#     }
+#     mgmnt = {
+#       port     = 9990
+#       protocol = "HTTP"
+#       forward = {
+#         target_group_key = "jboss-mgmt"
+#       }
+#     }
+#   }
 
-  depends_on = [module.jboss]
+#   depends_on = [module.jboss]
 
-  tags = {
-    Terraform = "true"
-  }
-}
+#   tags = {
+#     Terraform = "true"
+#   }
+# }
 
 
-resource "aws_route53_record" "jboss" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "jboss"
-  type    = "A"
+# resource "aws_route53_record" "jboss" {
+#   zone_id = data.aws_route53_zone.selected.zone_id
+#   name    = "jboss"
+#   type    = "A"
 
-  alias {
-    name                   = module.jboss_alb.dns_name
-    zone_id                = module.jboss_alb.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = module.jboss_alb.dns_name
+#     zone_id                = module.jboss_alb.zone_id
+#     evaluate_target_health = true
+#   }
+# }
--- 05_eks.tf
+++ 05_eks.tf
@@ -29,6 +29,7 @@ module "eks" {
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
+  enable_irsa                              = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -56,4 +57,49 @@ module "eks" {
     Environment = "dev"
     Terraform   = "true"
   }
+}
+
+
+module "lb_controller_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "aws-load-balancer-controller"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.lb_controller_role.iam_role_arn
+  }
+
+  depends_on = [module.eks]
 }
